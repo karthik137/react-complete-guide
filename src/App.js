979fc924473f44a763dd2736438ddf42502632c0
karthik <raokarthikg@gmail.com>
2020-07-28 08:09:28 +0530

    First component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     {/* <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Welcome to React</h1>
-    //     </header>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p> */}
-    //     <h1>Hi, i am a react app</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <h1>Hi, i am a react app</h1>
+        <Person></Person>
+      </div>
+    );
    
    // This will take h1 as text
    // return React.createElement('div', null, 'h1','Hi,iam react app');
@@ -24,7 +26,7 @@ class App extends Component {
    //return React.createElement('div', null, React.createElement('h1',null, 'Hi this is kirito'));
 
    //Apply css
-   return React.createElement('div', {className : 'App'}, React.createElement('h1',null, 'Hi this is kirito'));
+   //return React.createElement('div', {className : 'App'}, React.createElement('h1',null, 'Hi this is kirito'));
 
   }
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+/**
+ * A component is just a function which returns jsx
+ */
+
+ 
+//  function person() {
+//      return
+//  }
+
+// ES6 equivalent to function
+const person = () => {
+    return <p>I am a Person!</p>
+}
+
+
+export default person;
