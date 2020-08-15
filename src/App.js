00eb8b7e8e9f78087cacb27a0f1cb12b3cb579eb
karthik <raokarthikg@gmail.com>
2020-08-15 10:57:38 +0530

    List and keys

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ class App extends Component {
   state = {
     person : [
       {
+        id: '1',
         name : 'Kirito',
         age : 28
       },
       {
+        id: '2',
         name : 'Asuna',
         age : 29
       },
       {
+        id: '3',
         name : 'Eugeo',
         age : 26
       }
@@ -24,28 +27,44 @@ class App extends Component {
   }
 
 
-switchNameHandler = (name) => {
-  //console.log('Was clicked!');
-  //Do not change the state directly, use setState
-  //this.state.person[0].name = 'Kirito Kazuto';
-  this.setState(
-    {
-      person : [
-        {
-          name : name,
-          age : 28
-        },
-        {
-          name : 'Asuna',
-          age : 29
-        },
-        {
-          name : 'Eugeo',
-          age : 27
-        }
-      ]
-    }
-  )
+// switchNameHandler = (name) => {
+//   //console.log('Was clicked!');
+//   //Do not change the state directly, use setState
+//   //this.state.person[0].name = 'Kirito Kazuto';
+//   this.setState(
+//     {
+//       person : [
+//         {
+//           name : name,
+//           age : 28
+//         },
+//         {
+//           name : 'Asuna',
+//           age : 29
+//         },
+//         {
+//           name : 'Eugeo',
+//           age : 27
+//         }
+//       ]
+//     }
+//   )
+// }
+
+
+deletePersonHandler = (personIndex) => {
+  //const persons  = this.state.person;
+  // persons.splice(personIndex, 1);
+  // this.setState({
+  //   person: persons
+  // });
+  //create copy of the state, change the copy and then update the state
+  const persons = [...this.state.person];
+  persons.splice(personIndex, 1);
+  this.setState({
+    person: persons 
+  });
+
 }
 
 
@@ -99,13 +118,22 @@ togglePersonHandler = () => {
     if(this.state.showPersons){
       persons = (
         <div >
-        <Person 
-        name={this.state.person[1].name} 
-        age = {this.state.person[1].age}
-        click={this.switchNameHandler}
-        changed={this.nameChangeHandler}>
-          My Hobbies: Cooking</Person>
-        <Person name={this.state.person[2].name} age = {this.state.person[2].age}></Person>
+          {
+            this.state.person.map((person, index) => {
+              return <Person 
+                click={ () => this.deletePersonHandler(index)}
+                name={person.name} 
+                age={person.age}
+                key={person.id}/>
+            })
+          }
+          {/* <Person 
+          name={this.state.person[1].name} 
+          age = {this.state.person[1].age}
+          click={this.switchNameHandler}
+          changed={this.nameChangeHandler}>
+            My Hobbies: Cooking</Person>
+          <Person name={this.state.person[2].name} age = {this.state.person[2].age}></Person> */}
         </div>
       );
     }
@@ -114,7 +142,7 @@ togglePersonHandler = () => {
       <div className="App">
         <h1>Hi, i am a react app</h1>
         <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
-          <Person name={this.state.person[0].name} age = {this.state.person[0].age}></Person>
+          {/* <Person name={this.state.person[0].name} age = {this.state.person[0].age}></Person> */}
           {persons}
       </div>
     );
