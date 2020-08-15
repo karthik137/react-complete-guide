52b90dfa25d3c61fabc3c4eaaa67c571811e8266
karthik <raokarthikg@gmail.com>
2020-08-15 09:49:46 +0530

    Lists

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ class App extends Component {
         name : 'Eugeo',
         age : 26
       }
-    ]
+    ],
+    otherState: 'Some other Value',
+    showPersons: false
   }
 
 
@@ -69,21 +71,47 @@ nameChangeHandler = (event) => {
 
 }
 
+togglePersonHandler = () => {
+
+  //This will merge the state. NOT REPLACE
+  const doesShow = this.state.showPersons;
+  this.setState({
+    showPersons: !doesShow
+  });
+}
+
+
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+
     return (
       <div className="App">
         <h1>Hi, i am a react app</h1>
-        <button onClick={() => this.switchNameHandler('Kirito Daijobu')}>Switch Name</button>
+        <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
           <Person name={this.state.person[0].name} age = {this.state.person[0].age}></Person>
-        <Person 
-        name={this.state.person[1].name} 
-        age = {this.state.person[1].age}
-        click={this.switchNameHandler}
-        changed={this.nameChangeHandler}>
-          My Hobbies: Cooking</Person>
-        <Person name={this.state.person[2].name} age = {this.state.person[2].age}></Person>      
+        {
+          this.state.showPersons === true ?  
+          <div >
+          <Person 
+          name={this.state.person[1].name} 
+          age = {this.state.person[1].age}
+          click={this.switchNameHandler}
+          changed={this.nameChangeHandler}>
+            My Hobbies: Cooking</Person>
+          <Person name={this.state.person[2].name} age = {this.state.person[2].age}></Person>      
 
+
+          </div> : null
+        }
       </div>
     );
   
