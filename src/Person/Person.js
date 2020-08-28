fb7251ee58ad812659d1e8c341dacd4d0d8f2ca5
Kirito <raokarthik83@gmail.com>
2020-08-28 23:20:52 +0530

    Styled components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Person.css';
-import Radium from 'radium'
+//import Radium from 'radium'
+import styled from 'styled-components'
 /**
  * A component is just a function which returns jsx
  */
@@ -11,6 +12,22 @@ import Radium from 'radium'
 //  }
 
 // ES6 equivalent to function
+
+const StyledDiv = styled.div`
+.Person {
+    width: 70%;
+    margin: auto;
+    border: 2px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+}
+
+'@media (min-width: 500px)': {
+    width: '450px'
+}
+`
+
 const person = (props) => {
 const style = {
     '@media (min-width: 500px)': {
@@ -19,15 +36,21 @@ const style = {
 }
 // return <p>I am {props.name} a Person and I am {Math.floor(Math.random() * 20)} years old!</p>
 return (
-        <div className="Person" style={style}>
+        // <div className="Person" style={style}>
+        //     <p onClick={props.click}>I am {props.name} a Person and I am {props.age} years old!</p>
+        //     <p>{props.children}</p>
+        //     <input type="text" onChange={props.changed} value={props.name}/>
+        // </div>
+
+        <StyledDiv>
             <p onClick={props.click}>I am {props.name} a Person and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
 
-)
+        </StyledDiv>
+        )
 
 }
 
 
-export default Radium(person);
+export default person;
